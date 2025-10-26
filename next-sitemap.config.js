07c50b2819cb4ff6302ed77b544aeceaeacaa401
26farhaan <farhan.21144@gmail.com>
2025-10-26 20:58:35 +0700

    fix: fix site map and spacing bottom layout

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -3,13 +3,8 @@ module.exports = {
   siteUrl: "https://www.mohamadfarhan.space",
   generateRobotsTxt: true,
   alternateRefs: [
-    {
-      href: "https://www.mohamadfarhan.space/id",
-      hreflang: "id-ID",
-    },
-    {
-      href: "https://www.mohamadfarhan.space/en",
-      hreflang: "en-US",
-    },
+    { href: "https://example.com/", hreflang: "x-default" },
+    { href: "https://example.com/en", hreflang: "en" },
+    { href: "https://example.com/id", hreflang: "id" },
   ],
 };
